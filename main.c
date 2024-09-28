22d10c2920dab0b4575ab1eff8487f8e08e5dd6d
Samuele-Scandellari <samuscan01@gmail.com>
2024-09-28 16:12:44 +0200

    Esercizio 3.3

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int eta;
+    char nome;
+    printf("Calcoala se una persona è maggiorenne o meno\n Nome: ");
+    scanf("%c", &nome);
+    printf("Eta: ");
+    scanf("%d",&eta);
+    if (eta >=18);
+    printf("%c è maggiorenne\n",nome);
+}
